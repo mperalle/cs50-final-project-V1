0121e5352823f5df20126ad08ccf0fe9b76fabef
Marvin <marvin.peralle@gmail.com>
2024-01-31 12:22:28 +0000

    implement simple TCP echo server

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"io"
+	"log"
+	"net"
+)
+
+const ListAddr string = "localhost:3000"
+
 func main() {
 
+	//start TCP server listening on specified port
+	listener, err := net.Listen("tcp", ListAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer listener.Close()
+
+	//loop for accepting incoming connections
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		//test with an echo response
+		io.Copy(connection, connection)
+	}
 }
