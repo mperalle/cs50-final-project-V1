6a10c6f93735d6ba6d16c2bd8ad91477f2594a69
Marvin <marvin.peralle@gmail.com>
2024-01-31 14:08:08 +0000

    create slice of connections, create function to handler every connection with a goroutine and allow to chat between all connections

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"net"
 )
 
 const ListAddr string = "localhost:3000"
+
+// List of connections to keep track of
+var ConnectionList []net.Conn
 
 func main() {
 
@@ -15,6 +18,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("Start server on: ", listener.Addr().String())
 
 	defer listener.Close()
 
@@ -25,8 +29,28 @@ func main() {
 			log.Println(err)
 			continue
 		}
+		fmt.Println("New connection from: ", connection.RemoteAddr())
+		//add connection to list of connections
+		ConnectionList = append(ConnectionList, connection)
 
-		//test with an echo response
-		io.Copy(connection, connection)
+		//launch go routine for handling the connection
+		go connectionHandler(connection)
+	}
+}
+
+func connectionHandler(connection net.Conn) {
+	//create buffer to read from connection
+	buffer := make([]byte, 1024)
+	numberRead, err := connection.Read(buffer)
+	if err != nil {
+		log.Println(err)
+	}
+	message := buffer[0:numberRead]
+
+	//write to all other connections
+	for _, c := range ConnectionList {
+		if c != connection {
+			c.Write(message)
+		}
 	}
 }
